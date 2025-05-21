6924005ba935aa236d178745c7b04363ae7622dd
João Meneses Costa <joaomcosta37@outlook.com>
2025-05-21 04:39:33 -0300

    correção de bugs

--- js/function.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const mapas = document.querySelectorAll(".mapa iframe");
-    mapas.forEach(iframe => {
-        iframe.setAttribute("src", iframe.getAttribute("data-src"));
-    });
-});
