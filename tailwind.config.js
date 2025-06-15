62b71238d6f515ce3ef194d0aa8a1cd934b1e2d4
João Meneses Costa <joaomcosta37@outlook.com>
2025-06-14 22:59:48 -0300

    Implementação de TailwindCSS e PostCSS

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+            primary: '#0070C9',
+        primaryDark: '#005a8c'
+      },
+      backgroundImage: {
+        'servicos-banner': "url('../image/fb6456ee-c321-4e45-8b1c-5a8457ec0076.png')",
+      },
+      fontFamily: {
+        'sans': ['Arial', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
